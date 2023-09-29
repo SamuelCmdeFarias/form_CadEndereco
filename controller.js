6cb0c42e7df6062c650fcfb1be61f2efbec09386
Samuel <samucacsg@gmail.com>
2023-09-29 11:45:32 -0300

    func p preencher form c dados de endereco via cep

--- controller.js
+++ controller.js
@@ -19,4 +19,14 @@ const limparFormulario = () => {
 //Verifica se CEP é valido
 
 const eNumero = (numero) => /^[0-9]+$/.test(numero); //testa numero informado com expressão regular
-const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);//verifica tamanho do cep digitado e executa função de validação do cep eNumero
+
+//função para preencher formulário
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+
+}
